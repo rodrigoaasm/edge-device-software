0fa4cd9585a4cd3b9fb96edd7bff327c31e9a455
Rodrigo Aparecido Silva Maia <ramaia@cpqd.com.br>
2025-06-08 18:05:47 -0300

    Utilizando kustomize para gerenciar deployment

--- scripts/listen_results.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mosquitto_sub -d \
-  -h 10.42.0.126 -p 1883 \
-  -t deployments/#
-  
--- up.sh
+++ up.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-ED_OPERATOR_VERSION="0.3.0"
-NANOMQ_VERSION=latest
-INFLUXDB_VERSION="0.3.0"
-TELEGRAF_BRIDGE_VERSION="0.3.0"
-TELEGRAF_AGG_VERSION="0.3.0". 
-
 echo "creating roles"
 kubectl apply -f roles/role_manager.yaml
 kubectl apply -f roles/role_deployment.yaml
@@ -19,36 +13,10 @@ kubectl apply -f roles/role_manager_binding.yaml
 kubectl apply -f roles/role_deployment_binding.yaml
 echo "service account created"
 
-echo "create operator deployment"
-pushd "ed-operator" > /dev/null
-kustomize edit set image rodrigoasmaia/ed-operator=rodrigoasmaia/ed-operator:$ED_OPERATOR_VERSION
-kustomize build . | kubectl apply -f -
-
-echo "create services default:"
-
-echo "- nanomq"
-pushd "../deployments-default/nanomq" > /dev/null
-kustomize edit set image emqx/nanomq=emqx/nanomq:$NANOMQ_VERSION
-kustomize build . | kubectl apply -f -
-
-echo "- Influxdb"
-pushd "../influxdb" > /dev/null
+echo "creating pv for influxdb"
 kubectl apply -f ../../volumes-default/influxdb_pv.yaml
-kustomize edit set image influxdb:2.7.11-alpine=rodrigoasmaia/influxdb:$INFLUXDB_VERSION
-kustomize build . | kubectl apply -f -
 
-echo "- telegraf-bridge"
-pushd "../telegraf-bridge" > /dev/null
-kustomize edit set image rodrigoasmaia/telegraf-bridge=rodrigoasmaia/telegraf-bridge:$TELEGRAF_BRIDGE_VERSION
-kustomize build . | kubectl apply -f -
-
-echo "- telegraf-agg"
-pushd "../telegraf-agg" > /dev/null
-kustomize edit set image rodrigoasmaia/telegraf-agg=rodrigoasmaia/telegraf-agg:$TELEGRAF_AGG_VERSION
-kustomize build . | kubectl apply -f -
-
-echo "Opening external ports..."
-pushd "../.."
-kubectl apply -f nodeport-default/nodeports.yaml
+echo "creating services..."
+kubectl apply -k .
 
 kubectl get services -n ed-system
