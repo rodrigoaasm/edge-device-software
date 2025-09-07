fd57e124354353ec290dd6c30e19c09901cc0fc0
Rodrigo Aparecido Silva Maia <ramaia@cpqd.com.br>
2025-09-07 15:16:03 -0300

    Adicionando deviceid na mensagens do topico de resultados

--- deployments-ext/opc-ua-agent/internal/app/mqtt/mqtt-client.go
+++ deployments-ext/opc-ua-agent/internal/app/mqtt/mqtt-client.go
@@ -152,6 +152,7 @@ func (c *MQTTClient) publish(topic string, payload []byte) error {
 func (c *MQTTClient) PublishAck(CorrelationId string) error {
 	c.log.Info(fmt.Sprintf("Publishing ack: CorrelationId=%s", CorrelationId))
 	payload, err := json.MarshalIndent(dto.AckDto{
+		DeviceId:      c.config.DeviceId,
 		CorrelationId: CorrelationId,
 		Timestamp:     time.Now().Unix(),
 		Ack:           true,
@@ -167,6 +168,7 @@ func (c *MQTTClient) PublishResult(CorrelationId string, Success bool, Message s
 	timestamp := time.Now().Unix()
 	c.log.Info(fmt.Sprintf("Publishing result: CorrelationId=%s, Success=%v, Message=%s", CorrelationId, Success, Message))
 	payload, err := json.MarshalIndent(dto.ResultDto{
+		DeviceId:      c.config.DeviceId,
 		CorrelationId: CorrelationId,
 		Success:       Success,
 		Data:          Data,
